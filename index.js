758daeaba5a991a09c716211ce2d8ac4d497b53e
Arpita Roy <arpita.roy.workar@gmail.com>
2023-09-21 20:01:43 +0530

    update changes

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const menu = document.getElementById("menu-btn");
-
+const exitMenu = document.getElementById("exit-btn");
+const sidebar = document.getElementById("sidebar");
 menu.addEventListener("click", () => {
+  sidebar.style = console.log("clicked");
+});
+
+exitMenu.addEventListener("click", () => {
   console.log("clicked");
 });
+console.log(sidebar);
